b39c0995d08ce27f4f96a1ea871f14ca65feb78a
ekaterina <kat_klepko@mail.ru>
2023-02-18 21:18:08 +0300

    commit2

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num / 10;
+// int num2 = num1 % 10;
+
+// Console.WriteLine(num2);
+
+
+
+//  Напишите программу, которая выводит третью цифру заданного числа 
+//  или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num > 99)
+// {
+//     while (num >= 1000)
+//     {
+//         num = num / 10;
+//     }
+//     int num2 = num % 10;
+//     Console.WriteLine(num2);
+// }
+// else
+//     Console.WriteLine("Такой цифры нет");
+
+
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+
+// int num = new Random().Next(1, 8);
+// Console.WriteLine(num);
+
+// if(num > 5)
+//     Console.WriteLine("выходной");
+// else
+//     Console.WriteLine("Пора на работу!");
